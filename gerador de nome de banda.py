ebf60d09074a01896b89b7e3d8d25430b0943544
Thaisa-Cristina-Dev <94863117+Thaisa-Cristina-Dev@users.noreply.github.com>
2022-01-28 00:10:14 -0300

    Add files via upload

---
+++ gerador de nome de banda.py
@@ -0,0 +1,4 @@
+print("Bem-vindo ao Gerador de Nomes de Banda.")
+street = input("Qual é o nome da cidade em que você cresceu?\n")
+pet = input("Qual o nome do seu pet?\n")
+print("O nome da sua banda pode ser " + street + " " + pet)
